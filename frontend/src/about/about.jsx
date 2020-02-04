712fd91b9a607667080676d6bdf3fbf44f8d5c11
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-02-03 22:24:52 -0300

    Create PageHeader component and add on todo and about pages

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import PageHeader from '../template/pageHeader';
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="About" small="About Us"></PageHeader>
+    <h2>Nossa História</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro
+      repellat, laudantium nihil rem quas veritatis ex officia. Hic, nobis?
+      Alias nobis, nisi distinctio tempora necessitatibus sit unde impedit
+      possimus.
+    </p>
   </div>
 );
