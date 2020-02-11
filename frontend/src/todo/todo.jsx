9d9f14d3b9fd5ed6bcb5ffd4fe6fea6c5bbd2975
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-02-11 19:58:29 -0300

    Create action to mark todo as done and pending

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -15,6 +15,8 @@ export default class Todo extends Component {
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
 
     this.refresh();
   }
@@ -40,6 +42,18 @@ export default class Todo extends Component {
     this.setState({ ...this.state, description: e.target.value });
   }
 
+  handleMarkAsDone(todo) {
+    axios
+      .put(`${URL}/${todo._id}`, { ...todo, done: true })
+      .then(resp => this.refresh());
+  }
+
+  handleMarkAsPending(todo) {
+    axios
+      .put(`${URL}/${todo._id}`, { ...todo, done: false })
+      .then(resp => this.refresh());
+  }
+
   render() {
     return (
       <div>
@@ -49,7 +63,12 @@ export default class Todo extends Component {
           handleChange={this.handleChange}
           description={this.state.description}
         />
-        <TodoList list={this.state.list} handleRemove={this.handleRemove} />
+        <TodoList
+          list={this.state.list}
+          handleRemove={this.handleRemove}
+          handleMarkAsDone={this.handleMarkAsDone}
+          handleMarkAsPending={this.handleMarkAsPending}
+        />
       </div>
     );
   }
